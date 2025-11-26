d95d0821b035b3887e5319b4fe7e05f029f84818
owuorviny109 <owuorvincent958@gmail.com>
2025-11-26 11:32:24 +0300

    Register 'members' app in INSTALLED_APPS in settings.py

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'members',  # Register the 'members' app
 ]
 
 MIDDLEWARE = [
