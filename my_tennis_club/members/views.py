2f08a2417a3177080a816d377fc1f5b6ac603bcb
owuorviny109 <owuorvincent958@gmail.com>
2025-11-26 11:16:40 +0300

    Update members view with welcome message and comments

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+"""
+Views for the 'members' app.
 
-# Create your views here.
+Purpose:
+- Handle HTTP requests and return HTTP responses.
+- Serve as the controller layer between models/templates and the user.
+"""
+
+from django.shortcuts import render          # Shortcut for rendering templates
+from django.http import HttpResponse         # To return raw HTTP responses
+
+# Basic view for members homepage
+def members(request):
+    """
+    Purpose: Handle requests to the members homepage.
+
+    Function:
+    - Accepts an HTTP request object.
+    - Returns a plain-text HTTP response.
+    - Can later be extended to render templates with dynamic data.
+    """
+    return HttpResponse(
+        "Hello from the members app! "
+        "All members are welcome here. "
+        "It is my tennis club. Enjoy your stay!"
+    )
